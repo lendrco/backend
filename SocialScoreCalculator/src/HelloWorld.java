a08fb74c962a89c7b5ef23881a8dda0c2e417679
Gjorgji Gjeorgjievski <gjorgji@66-192-186-66.static.twtelecom.net>
2013-04-06 18:07:15 -0700

    Merging changes with MongoDb hello world

--- SocialScoreCalculator/src/HelloWorld.java
+++ SocialScoreCalculator/src/HelloWorld.java
@@ -2,14 +2,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
-import java.io.IOException;
- 
+import java.io.IOException; 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import com.mongodb.MongoClient;
+import com.mongodb.DB;
+import java.util.Set;
  
 public class HelloWorld extends AbstractHandler
 {
@@ -23,33 +22,40 @@ public class HelloWorld extends AbstractHandler
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         response.getWriter().println("<h1>Hello World</h1>");
+        
+        testDb(response);
     }
  
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
         server.setHandler(new HelloWorld());
-        
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = null;
-        try{
-           tx = session.beginTransaction();
-           List employees = session.createQuery("FROM Employee").list(); 
-           for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-              Employee employee = (Employee) iterator.next(); 
-              System.out.print("First Name: " + employee.getFirstName()); 
-              System.out.print("  Last Name: " + employee.getLastName()); 
-              System.out.println("  Salary: " + employee.getSalary()); 
-           }
-           tx.commit();
-        }catch (Hibernate e) {
-           if (tx!=null) tx.rollback();
-           e.printStackTrace(); 
-        }finally {
-           session.close(); 
-        }
-        
         server.start();
         server.join();
+    }
+    
+    public static void testDb(HttpServletResponse response)
+    {
+    	try {
+	    	MongoClient mongoClient = new MongoClient( "localhost" , 28017 );
+	    	DB db = mongoClient.getDB( "mydb" );
+	    	Set<String> colls = db.getCollectionNames();
+	
+	    	response.getWriter().println("MongoDb collections:");
+	    			
+	    	for (String s : colls) {
+	    		response.getWriter().println(s);
+	    	}
+	    	
+	    	response.getWriter().println("");
+    	}
+    	catch(Exception e) {
+    		try {
+				response.getWriter().println(e.getMessage());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+    	}
+    	
     }
 }
